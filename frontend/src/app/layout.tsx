416ca04e2e2d6563255b0c2d8b175964bff3d443
ALEXANDRE_HP <alexandre_hp@tcm.go>
2026-06-25 15:26:57 -0300

    fix: Correção para deploy

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,21 +8,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="pt-BR">
-      <head>
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=JetBrains+Mono:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
